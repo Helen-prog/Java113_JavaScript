e7306be6285629497c37c0c200e17a62017c78cb
Helen <elena.kozovyakina@gmail.com>
2024-07-29 12:57:37 +0300

    js

--- script.js
+++ script.js
@@ -1586,6 +1586,27 @@ console.log(typeof(message)); */
 // let personString = JSON.stringify(person);  // JS -> JSON
 // console.log(personString);
 
-fetch('https://jsonplaceholder.typicode.com/todos')
-    .then(response => response.json())  // JSON => JS
-    .then(json => console.log(json));
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//     .then(response => response.json())  // JSON => JS
+//     .then(json => console.log(json));
+
+
+document.querySelector("#load").addEventListener("click", load);
+
+function load(){
+    let url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            let ul = document.querySelector("#list");
+            let html = data.map(function(item){
+                return "<li>" + item.id + " " + item.name + " " + item.email + "</li>";
+            })
+            ul.insertAdjacentHTML("afterbegin", html.join(" "));
+        })
+}
+
+// let box = document.querySelector("div");
+// console.log(box.getBoundingClientRect().right);
